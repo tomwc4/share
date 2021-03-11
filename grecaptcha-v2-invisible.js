ec4b77aa052b979c18562acf675893d808371513
Tom Benway <tom@vectordefector.com>
2021-03-11 15:30:09 -0700

    Update grecaptcha-v2-invisible.js

--- grecaptcha-v2-invisible.js
+++ grecaptcha-v2-invisible.js
@@ -3,13 +3,13 @@
 
 jQuery(document).ready(function($){ 
 //check if recaptcha field exists 
-  if ($ ('.ginput_recaptcha'). length) { 
-    $('.input'). on('click' , function() { 
-      console.log ('check recaptcha');
+  if ($('.ginput_recaptcha').length) { 
+    $('.input').on('click', function() { 
+      //console.log ('check recaptcha');
       //activate v2 invisible recaptcha
       grecaptcha.execute(); 
     });
   } else { 
-    console.log ( 'has no recaptcha' ); 
+   // console.log ( 'has no recaptcha' ); 
   } 
 })
