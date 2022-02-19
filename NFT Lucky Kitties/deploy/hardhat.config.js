af95f667fcf8c8681088f123c049167f83c65d4a
Tom Benway <tom@vectordefector.com>
2022-02-18 17:25:05 -0700

    Add files via upload
    updated with default gas limits and full network list

--- NFT Lucky Kitties/deploy/hardhat.config.js
+++ NFT Lucky Kitties/deploy/hardhat.config.js
@@ -3,9 +3,12 @@ require("@nomiclabs/hardhat-ganache");
 require("@nomiclabs/hardhat-etherscan");
 require('dotenv').config();
 require("./scripts/deploy.js");
-require("./test/test.js");
+//require("./test/test.js");
 
-const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY, ETHERSCAN_KEY } = process.env;
+const { ALCHEMY_KEY_LIVE, ALCHEMY_KEY_DEV, ACCOUNT_PRIVATE_KEY, ETHERSCAN_KEY } = process.env;
+
+//const ALCHEMY_KEY = ALCHEMY_KEY_DEV;
+const ALCHEMY_KEY = ALCHEMY_KEY_LIVE;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -16,20 +19,20 @@ module.exports = {
   paths: {
     artifacts: './src/artifacts',
   },
-  defaultNetwork: "ganache",
+  defaultNetwork: "polygon",
   networks: {
     ganache: {
       url: 'HTTP://127.0.0.1:7545',
       chainId: 1337,
-      gasLimit: 6000000,
-    },/*
+      gasPrice: 50000000000 //set test gas to mimic live
+    }, 
     goerli: {
       url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_KEY}`,
       accounts: [`${ACCOUNT_PRIVATE_KEY}`]
     },
     mumbai: {
       url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_KEY}`,
-      accounts: [`${ACCOUNT_PRIVATE_KEY}`]
+      accounts: [`${ACCOUNT_PRIVATE_KEY}`],
     },
     ropsten: {
       url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_KEY}`,
@@ -42,13 +45,15 @@ module.exports = {
     ethereum: {
       chainId: 1,
       url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
-      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`],
+      gasPrice: 50000000000 //(in wei) = 50 gwei / changes frequently, check pricing!
     },
-    polgon: {
+    polygon: {
       url: `https://polygon-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`,
-      accounts: [`${ACCOUNT_PRIVATE_KEY}`]
+      accounts: [`${ACCOUNT_PRIVATE_KEY}`],
+      gasPrice: 35000000000 //(in wei) = 40 gwei / changes frequently, check pricing!
     },
-  */},
+  },
   etherscan: {
     apiKey: ETHERSCAN_KEY,
   },
